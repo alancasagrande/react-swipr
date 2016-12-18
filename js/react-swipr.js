084968861a145a409981db31ab371c6d5cbd989a
Alan Casagrande <alancasagrande@gmail.com>
2016-12-18 14:29:12 -0200

    add previousNavElement and nextNavElement props

--- js/react-swipr.js
+++ js/react-swipr.js
@@ -15,7 +15,9 @@ var ReactSwipr = React.createClass({
 
   propTypes: {
     elementId: React.PropTypes.string.isRequired,
-    navigation: React.PropTypes.bool
+    navigation: React.PropTypes.bool,
+    previousNavElement: React.PropTypes.element,
+    nextNavElement: React.PropTypes.element
   },
 
   componentDidUpdate: function() {
@@ -48,10 +50,10 @@ var ReactSwipr = React.createClass({
       navigation = (
         React.createElement("div", null,
           React.createElement("span", {className: "swipr_prev"},
-            React.createElement("svg", {xmlns: "http://www.w3.org/2000/svg", width: "50", height: "50", viewBox: "0 0 501.5 501.5"}, React.createElement("g", null, React.createElement("path", {fill: "#ff6700", d: "M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z"})))
+            this.props.previousNavElement || React.createElement("svg", {xmlns: "http://www.w3.org/2000/svg", width: "50", height: "50", viewBox: "0 0 501.5 501.5"}, React.createElement("g", null, React.createElement("path", {fill: "#ff6700", d: "M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z"})))
           ),
           React.createElement("span", {className: "swipr_next"},
-            React.createElement("svg", {xmlns: "http://www.w3.org/2000/svg", width: "50", height: "50", viewBox: "0 0 501.5 501.5"}, React.createElement("g", null, React.createElement("path", {fill: "#ff6700", d: "M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z"})))
+            this.props.nextNavElement ||React.createElement("svg", {xmlns: "http://www.w3.org/2000/svg", width: "50", height: "50", viewBox: "0 0 501.5 501.5"}, React.createElement("g", null, React.createElement("path", {fill: "#ff6700", d: "M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z"})))
           )
         )
       );
